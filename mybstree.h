6eae78c68b2e5e61ab878cfa1387117d969f7281
joshuapondrom <japrkf@mst.edu>
2017-04-14 04:38:44 +0000

    added more functions

--- mybstree.h
+++ mybstree.h
@@ -3,14 +3,14 @@
 //
 
 #include <climits>
+#include "abstractbstree.h"
 
 template < typename T >
 class Node{
-  private:
+  public:
     T data;
     Node* left;
     Node* right;
-  public:
     Node(T pdata = 0, Node* pleft = NULL, Node* pright = NULL){
       data = pdata;
       left = pleft;
@@ -24,7 +24,7 @@ class Node{
 };
 
 template< typename T >
-class MyBSTree:public AbstractBSTree{
+class MyBSTree:public AbstractBSTree<T>{
   private:
     Node<T>* root;
     int tsize;
@@ -43,7 +43,7 @@ class MyBSTree:public AbstractBSTree{
         return true;
       return false;
     }
-
+     
     int height() const{
       Node<T>* temp = root;
       int theight = 1;
@@ -56,30 +56,97 @@ class MyBSTree:public AbstractBSTree{
     
     const T& getMax() const throw(Oops){
       Node<T>* temp = root;
-      try{
         if(root == NULL)
 	 throw Oops("OOPS! : Tree is empty!!");
-      }
       while(temp->right != NULL){temp = temp->right;}
-      return right->data;
+      return temp->data;
     }
     
     const T& getMin() const throw(Oops){
       Node<T>* temp = root;
-      try{
         if(root == NULL)
 	 throw Oops("OOPS! : Tree is empty!!");
-      }
       while(temp->left != NULL){temp = temp->left;}
       return temp->data;
     }
 
-    int find(const T& x) const{return mfind(x)}
-    int mfind(const T& x, Node<T>* curr = root){
+    int find(const T& x) const{return mfind(x, root);}
+    int mfind(const T& x, Node<T>* curr){
       if(curr->data == x)
         return 1;
       return 1 + mfind(x,curr->left);
       return 1 + mfind(x,curr->right);
       return -1 * height();
+    }
+    
+    void clear(){mclear(root);}
+    void mclear(Node<T>* curr){
+      if(curr.isChild()){
+        delete curr;
+	return;
+      }
+      mclear(curr->left);
+      mclear(curr->right);
+      delete curr;
+    }
+    
+    void insert(const T& x){minsert(x,root);}
+    void minsert(const T& x, Node<T>* curr){
+      if(curr == NULL){
+        root = new Node<T>(x);
+	return;
+      }
+      if(curr->data == x){
+        return;
+      }
+      if(curr->data > x){
+        minsert(x, curr->left);
+      }
+      if(curr->data < x){
+        minsert(x,curr->right);
+      }
+    }
+    
+    void remove(const T& x){mremove(x,root);}
+    void mremove(const T& x, Node<T>* curr){
+      if(x == root){}
+    }
+
+    void printPreOrder() const {mprintPreOrder(root);}
+    void mprintPreOrder(Node<T>* curr){
+      if(curr->isChild())
+        return;
+      cout << curr->data;
+      mprint(curr->left);
+      mprint(curr->right);
+    }
+
+    void printPostOrder() const {mprintPostOrder(root);}
+    void mprintPostOrder(Node<T>* curr){
+      if(curr->isChild())
+        return;
+      mprint(curr->left);
+      mprint(curr->right);
+      cout << curr->data;
+    }
+
+    void print() const {mprint(root);}
+    void mprint(Node<T>& curr){
+      if(curr->isChild())
+        return;
+      mprint(curr->left);
+      cout << curr->data;
+      mprint(curr->right);
+    }
+
+    void prettyPrint (const Node<T>* t, const int pad) const {
+      string s(pad, ' ');
+      if ( t == NULL)
+        cout << endl;
+      else{
+        prettyPrint(t->right, pad+4);
+	cout << s << t->data << endl;
+	prettyPrint(t->left, pad+4);
+      }
     }
 };
